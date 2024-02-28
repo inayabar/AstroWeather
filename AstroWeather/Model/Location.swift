a0a5556a29eee1f266371d5f17ceebf1c1cd2857
Iñaki <inayabarb@gmail.com>
2024-02-28 12:27:40 -0300

    Add LocationWeather view and viewModel

--- AstroWeather/Model/Location.swift
+++ AstroWeather/Model/Location.swift
@@ -11,4 +11,12 @@ struct Location: Identifiable, Hashable {
     let name: String
     let latitude: Double
     let longitude: Double
+    
+    static let list: [Location] = [
+        Location(id: UUID(), name: "London", latitude: 51.5074, longitude: -0.1278),
+        Location(id: UUID(), name: "Buenos Aires", latitude: -34.6037, longitude: -58.3816),
+        Location(id: UUID(), name: "Montevideo", latitude: -34.9011, longitude: -56.1645),
+        Location(id: UUID(), name: "New York", latitude: 40.7128, longitude: -74.0060),
+        Location(id: UUID(), name: "Tokyo", latitude: 35.6895, longitude: 139.6917)
+    ]
 }
