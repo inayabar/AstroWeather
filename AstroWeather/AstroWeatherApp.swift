d1f7a518493255bc426708352068a1353c1672bf
Iñaki <inayabarb@gmail.com>
2024-02-28 11:36:59 -0300

    Initial commit: Create project, add readme, min iOS 14

---
+++ AstroWeather/AstroWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroWeatherApp.swift
+//  AstroWeather
+//
+//  Created by Iñaki Yabar Bilbao on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AstroWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
